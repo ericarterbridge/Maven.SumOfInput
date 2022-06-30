33443a95c82b7e0e8ccf57ecbdd6b31d10bde7a7
Eric <eric@Zipcoders-MacBook-Pro-6.local>
2022-06-30 13:49:46 -0400

    Finished lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+
+
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +9,16 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner getNum = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int n = getNum.nextInt();
+        int sum = 0;
+            for (int i = n; i >= 0; i--) {
+                sum += i;
+                //System.out.println("The sum is: "+sum);
+            }
+            System.out.println("The sum is: " + sum);
+
+        }
     }
-}
+
